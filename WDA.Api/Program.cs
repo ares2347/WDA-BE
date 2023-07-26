ee78e186fb51e6c5caadf356b0b4c9dc63fd9ad5
hoangtu.tran <trantu2000hp@gmail.com>
2023-07-27 01:16:33 +0700

    revert

--- WDA.Api/Program.cs
+++ WDA.Api/Program.cs
@@ -61,7 +61,7 @@ public class Program
         {
             EnableSsl = true,
             UseDefaultCredentials = false,
-            Credentials = new NetworkCredential(AppSettings.Instance.Smtp.Email, AppSettings.Instance.Smtp.Password, "outlook.com")
+            Credentials = new NetworkCredential(AppSettings.Instance.Smtp.Email, AppSettings.Instance.Smtp.Password)
         });
         builder.Services.AddScoped<IEmailService, EmailService>();
         
